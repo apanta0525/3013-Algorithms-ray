7ad24c6046839454f99986278e2bfdf42f3c2efe
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2023-01-25 18:58:34 -0600

    Update main.cpp

--- Assignments/A03/main.cpp
+++ Assignments/A03/main.cpp
@@ -1,15 +1,64 @@
+/*****************************************************************************
+*                    
+*  Author:           Sudhir Ray
+*  Email:            raysudhir30@gmail.com/ sray0228@my.msutexas.edu  
+*  Label:            A03
+*  Title:            Basic Project Organization
+*  Course:           CMPS 3013
+*  Semester:         Spring 2023
+* 
+*  Description:
+*        This program implements the concept of circular array queue. The program has a push function to add items in the queue 
+*        as well as a pop function to remove items from the queue. The program also prints all the items in the queue at the end.
+*       
+* 
+*  Usage:
+*       - create instance of CircularArrayQue
+*       - push function to add item into the queue
+*       - pop function to remove item from the queue
+*  
+* 
+*  Files:           
+*        main.cpp      :        driver program 
+*****************************************************************************/
+
+
 #include <iostream>
 
 using namespace std;
 
-
+/**
+ * CircularArrayQue
+ * 
+ * Description:
+ *      This is a class that demontrates the concept of circular array queue. It has public methods like push and pop 
+ *      to add and remove items from the queue simultaneously. 
+ * Public Methods:
+ *                              CircularArrayQue()
+ *                              CircularArrayQue(int)
+ *          void                Push(int item)
+ *          int                 Pop()
+ *          friend ostream      &operator << (ostream &, const CircularArrayQue)            
+ * 
+ * Private Methods:
+ *          void                init(int size = 0)
+ *          bool                Full()
+ * 
+ * Usage: 
+ * 
+ *  CircularArrayQue()  // creates an instance of CircularArrayQue
+ *  push (int item)     // inserts item in the queue 
+ *  pop ()              // removes item from the queue
+ *
+ */
 class CircularArrayQue {
 private:
-    int *Container;
-    int Front;
-    int Rear;
-    int QueSize; // items in the queue
-    int CurrentSize;
+    int *Container;    //pointer pointing to the items in the list
+    int Front;         // item at the front of the queue
+    int Rear;          // item at the back of the queue
+    int QueSize;       // size of the queue
+    int CurrentSize;   // the current size of the queue
+    
     void init(int size = 0) {
         Front = Rear = CurrentSize = 0;
         QueSize = size;
@@ -28,6 +77,20 @@ public:
         Container = new int[size];
         init(size);
     }
+    
+    /**
+     * Public : push
+     * 
+     * Description:
+     *      The function adds item in the queue
+     * 
+     * Params:
+     *      int     :  the item to be added in the queue.
+     * 
+     * Returns:
+     *      void
+     */
+
 
     void Push(int item) {
         if (!Full()) {
@@ -38,6 +101,20 @@ public:
             cout << "FULL!!!!" << endl;
         }
     }
+    
+    /**
+     * Public : pop
+     * 
+     * Description:
+     *      The function removes item from the front of the queue.
+     * 
+     * Params:
+     *     None
+     * 
+     * Returns:
+     *      int : the item that has been removed from the queue.
+     */
+
 
     int Pop() {
         int temp = Container[Front];
@@ -45,6 +122,9 @@ public:
         CurrentSize--;
         return temp;
     }
+    
+    
+    
     friend ostream &operator<<(ostream &os, const CircularArrayQue &other);
 };
 
